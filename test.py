55a91d8e844e255d34022461c88f97fbf59f70ec
test01 <test01@kami666.xyz>
2026-01-04 16:30:30 +0800

    docs: update promotional page with new features

--- test.py
+++ test.py
@@ -3,7 +3,5 @@ import requests
 
 worker_url = "ip.kami666.xyz"
 cf_response = cfspider.get("https://httpbin.org/ip", cf_proxies=worker_url)
-req_response = requests.get("https://httpbin.org/ip")
 
 print(cf_response.text)
-print(req_response.text)
